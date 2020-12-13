3fe58fd2e974119e106904ac35224265cde973f3
aviral243 <aviralgangwar24@gmail.com>
2020-12-13 20:49:44 +0530

    save google id to collection

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
 
+const User = mongoose.model("users");
 passport.use(
   new GoogleStrategy(
     {
@@ -10,7 +12,9 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, refreshToken, profile, done);
+      new User({
+        googleId: profile.id,
+      }).save();
     }
   )
 );
