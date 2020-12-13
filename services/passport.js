382f6ba657c428f4952f5640f9c3492abd0b1f5a
aviral243 <aviralgangwar24@gmail.com>
2020-12-13 22:50:57 +0530

    auth finish

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,17 @@ const keys = require("../config/keys");
 const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -12,9 +23,19 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({
-        googleId: profile.id,
-      }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({
+            googleId: profile.id,
+          })
+            .save()
+            .then((user) => {
+              done(null, user);
+            });
+        }
+      });
     }
   )
 );
