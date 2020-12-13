1bf34071b2627e80c89ebecfce35d56ad1c2a0ba
aviral243 <aviralgangwar24@gmail.com>
2020-12-13 16:45:42 +0530

    mongoose connection

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 require("./routes/authRoutes")(app);
 
