69aa167a9d2985e980d00a13b22f6204931838bc
aviral243 <aviralgangwar24@gmail.com>
2020-12-16 15:26:01 +0530

    setup reducer

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div>
+          <div className="nav-wrapper">
+            <a className="left brand-logo">Emaily</a>
+            <ul className="right">
+              <li>
+                <a>Login with Google</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import reduxThunk from "redux-thunk";
+
+import "materialize-css/dist/css/materialize.min.css";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    {" "}
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
