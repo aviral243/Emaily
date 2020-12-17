763be48b8188591d3e06458f98ec918d79adfa22
aviral243 <aviralgangwar24@gmail.com>
2020-12-18 02:23:03 +0530

    stripe backend route

--- client/src/components/StripeWrapper.js
+++ client/src/components/StripeWrapper.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
+import * as actionTypes from "../actions";
 
 class StripeWrapper extends Component {
   render() {
     return (
       <StripeCheckout
         amount={500}
-        token={(token) => console.log(token)}
+        token={(token) => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+        name="Emaily"
+        description="$5 for 5 email credits"
+      >
+        <button className="btn">Add credits</button>
+      </StripeCheckout>
     );
   }
 }
 
-export default StripeWrapper;
+export default connect(null, actionTypes)(StripeWrapper);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
+const bodyParser = require("body-parser");
 require("./models/User");
 require("./services/passport");
 
@@ -12,6 +13,8 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -24,6 +27,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
